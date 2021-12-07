7a200a5639a03fc0c7b0e030fab1a2b6107385d5
Andrew <920076768@qq.com>
2021-12-07 23:52:46 +0800

    modify Exercise1.37 so that cont-frac-iterative can be reused

--- chapter1/section1-3/Exercise1.37.rkt
+++ chapter1/section1-3/Exercise1.37.rkt
@@ -1,22 +1,16 @@
-#lang sicp
+#lang racket
 
 ;Exercise1.37
 
 ;cont-frac iterative
+(#%provide cont-frac-iterative)
+(#%require sicp)
 (define (cont-frac-iterative n d k)
   (define (iter i result)
     (if (< i 1)
         result
         (iter (- i 1) (/ (n i) (+ result (d i))))))
   (iter k 0))
-
-;1/phi is 0.6180339887498948 calculated by python 2 / (1 + 5**0.5)
-(cont-frac-iterative (lambda (i) 1.0)
-           (lambda (i) 1.0)
-           10) ;0.6179775280898876
-(cont-frac-iterative (lambda (i) 1.0)
-           (lambda (i) 1.0)
-           11) ;0.6180555555555556, so k must be ge 11, to get an approximation that is accurate to 4 decimal places
 
 ;cont-frac recursive
 (define (cont-frac-recursive n d k)
@@ -26,10 +20,20 @@
         (/ (n a) (+ (d a) (recurse (+ a 1))))))
   (recurse 1))
 
-;1/phi is 0.6180339887498948 calculated by python 2 / (1 + 5**0.5)
-(cont-frac-recursive (lambda (i) 1.0)
-           (lambda (i) 1.0)
-           10) ;0.6179775280898876
-(cont-frac-recursive (lambda (i) 1.0)
+(module+ main
+  ;1/phi is 0.6180339887498948 calculated by python 2 / (1 + 5**0.5)
+  (cont-frac-iterative (lambda (i) 1.0)
+                       (lambda (i) 1.0)
+                       10) ;0.6179775280898876
+  (cont-frac-iterative (lambda (i) 1.0)
            (lambda (i) 1.0)
            11) ;0.6180555555555556, so k must be ge 11, to get an approximation that is accurate to 4 decimal places
+
+  ;1/phi is 0.6180339887498948 calculated by python 2 / (1 + 5**0.5)
+  (cont-frac-recursive (lambda (i) 1.0)
+                       (lambda (i) 1.0)
+                       10) ;0.6179775280898876
+  (cont-frac-recursive (lambda (i) 1.0)
+                       (lambda (i) 1.0)
+                       11) ;0.6180555555555556, so k must be ge 11, to get an approximation that is accurate to 4 decimal places
+  )
