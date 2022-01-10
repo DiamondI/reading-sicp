8ec1dac0cc0b602054b4feecb0e15df21fac02a2
Andrew <920076768@qq.com>
2022-01-10 14:49:27 +0800

    add Exercise2.2

---
+++ chapter2/section2-1/Exercise2.2.rkt
@@ -0,0 +1,50 @@
+#lang racket
+
+(require sicp)
+
+;Exercise2.1
+
+(define (print-point p)
+    (newline)
+    (display "(")
+    (display (x-point p))
+    (display ", ")
+    (display (y-point p))
+    (display ")")
+    )
+
+(define (make-point x y)
+    (cons x y))
+
+(define (x-point p)
+    (car p))
+
+(define (y-point p)
+    (cdr p))
+
+(define (make-segment start end)
+    (cons start end))
+
+(define (start-segment segment)
+    (car segment))
+
+(define (end-segment segment)
+    (cdr segment))
+
+(define (midpoint-segment segment)
+    (let ((start (start-segment segment))
+          (end (end-segment segment)))
+          (make-point (/ (+ (x-point start) (x-point end)) 2) (/ (+ (y-point start) (+ (y-point end))) 2))
+          ))
+
+(module+ main
+    (print-point (make-point 1 2))
+    (let ((start (make-point 1 2))
+         (end (make-point 3 4)))
+         (print-point (midpoint-segment (make-segment start end)))
+         )
+    (let ((start (make-point 2 3))
+         (end (make-point 3 4)))
+         (print-point (midpoint-segment (make-segment start end)))
+         )
+    )
