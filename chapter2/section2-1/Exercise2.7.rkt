d110361462ac49fd5197a138837e9dc5c66770f1
DiamondI <920076768@qq.com>
2022-01-16 23:22:52 +0800

    add Exercise 2.7, 2.8

---
+++ chapter2/section2-1/Exercise2.7.rkt
@@ -0,0 +1,56 @@
+#lang racket
+
+(require sicp)
+(provide make-interval add-interval mul-interval div-interval lower-bound upper-bound print-interval)
+
+(define (make-interval a b) (cons a b))
+
+(define (add-interval x y) 
+  (make-interval (+ (lower-bound x)
+                    (lower-bound y))
+                 (+ (upper-bound x)
+                    (upper-bound y))))
+
+(define (mul-interval x y) 
+  (let ((p1 (* (lower-bound x)
+               (lower-bound y))) 
+		(p2 (* (lower-bound x)
+                       (upper-bound y))) 
+		(p3 (* (upper-bound x)
+                       (lower-bound y))) 
+		(p4 (* (upper-bound x)
+                       (upper-bound y)))) 
+	(make-interval (min p1 p2 p3 p4)
+                       (max p1 p2 p3 p4))))
+
+(define (div-interval x y) 
+  (mul-interval x
+                (make-interval
+                 (/ 1.0 (upper-bound y))
+                 (/ 1.0 (lower-bound y)))))
+
+(define (lower-bound interval)
+  (car interval))
+
+(define (upper-bound interval)
+  (cdr interval))
+
+(define (print-interval interval)
+  (let ((a (lower-bound interval))
+        (b (upper-bound interval)))
+    (display "(")
+    (display a)
+    (display ", ")
+    (display b)
+    (display ")")))
+
+(module+ main
+  (define interval1 (make-interval 1.0 2.0))
+  (define interval2 (make-interval -2.0 3.0))
+  (display "Add interval: ")(newline)
+  (print-interval (add-interval interval1 interval2)) (newline)
+  (display "Mul interval: ") (newline)
+  (print-interval (mul-interval interval1 interval2)) (newline)
+  (display "Div interval: ") (newline)
+  (print-interval (div-interval interval1 interval2)) (newline)
+  )
---
+++ chapter2/section2-1/Exercise2.8.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(require sicp)
+(require "Exercise2.7.rkt")
+
+(define (sub-interval x y)
+  (add-interval x
+                (make-interval
+                 (* -1 (upper-bound y))
+                 (* -1 (lower-bound y)))))
+
+(module+ main
+  (define interval1 (make-interval 1.0 2.0))
+  (define interval2 (make-interval -2.0 3.0))
+  (display "Sub interval: ") (newline)
+  (print-interval (sub-interval interval1 interval2))
+  )
