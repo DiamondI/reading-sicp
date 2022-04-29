5f1a68c4f6f233166985e717eba3ad2b65c42fcb
Andrew <920076768@qq.com>
2022-04-29 23:39:14 +0800

    add Exercise 2.46 and 2.47

---
+++ chapter2/section2-2/Exercise2.46.rkt
@@ -0,0 +1,43 @@
+#lang racket
+
+(require sicp)
+
+(define (make-vect x y)
+  (list x y))
+
+(define (xcor-vect vect)
+  (car vect))
+
+(define (ycor-vect vect)
+  (cadr vect))
+
+(define (binary-op op v1 v2)
+  (let ((x1 (xcor-vect v1))
+        (x2 (xcor-vect v2))
+        (y1 (ycor-vect v1))
+        (y2 (ycor-vect v2)))
+    (make-vect (op x1 x2) (op y1 y2))))
+
+(define (add-vect v1 v2)
+  (binary-op + v1 v2))
+
+(define (sub-vect v1 v2)
+  (binary-op - v1 v2))
+
+(define (scale-vect s v)
+  (let ((x (xcor-vect v))
+        (y (ycor-vect v)))
+    (make-vect (* s x) (* s y))))
+
+(module+ main
+  (define v (make-vect 1 2))
+  (define u (make-vect 3 4))
+  (display v)
+  (newline)
+  (display (make-vect (xcor-vect v) (ycor-vect v)))
+  (newline)
+  (display (add-vect v u))
+  (newline)
+  (display (sub-vect u v))
+  (newline)
+  (display (scale-vect 3 u)))
---
+++ chapter2/section2-2/Exercise2.47(1).rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(require sicp)
+
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame frame)
+  (car frame))
+
+(define (edge1-frame frame)
+  (cadr frame))
+
+(define (edge2-frame frame)
+  (caddr frame))
+
